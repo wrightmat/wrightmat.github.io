da3d3b166d0adf04c48656a3b0c6b9dfce1738fb
Matt <wright.matt.a@gmail.com>
2022-12-13 13:24:19 -0600

    Barebones working initiative

--- dnd/initiative.js
+++ dnd/initiative.js
@@ -1,13 +1,39 @@
+// Look up character death saves if hp = 0 (in character json as deathSaves)
+// Could look up conditions for each character (conditions array in json) - but this might also be too taxing on the endpoint. Maybe have a toggle or something.
+// Have option to show monster and character (separately) images. Character image in decorations.avatarUrl. Monster image in avatarUrl (with an extra .com for some reason), but need to do more digging for the generic creature type version (typeId is creature type?)
+
+var letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' ];
+var encounter = {};
+var combatants = [];
+var opt_show_monster_name = false;
+var opt_show_monster_hp = false;
 
 function init() {
-  console.log(getEncounterDDB());
+  encounter = getFromDDB("encounter", "54d34b6e-393a-4791-9ece-593b9c5745b1");
+console.log(encounter);
+  combatants = buildCombatants();
+console.log(combatants);
+  populateOutput();
 }
 
-function getEncounterDDB() {
-  var r
+function getFromDDB(type, id) {
+  var r;
+  var u;
+  var d = new Object();
+  d.id = id;
+
+  if (type == "monster") {
+    u = "https://eogz05cvn9wh74f.m.pipedream.net"
+  } else if (type == "character") {
+    u = "https://eo3ofofdoqq9rgh.m.pipedream.net"
+  } else { // encounter
+    u = "https://eozorsz5jeuanh9.m.pipedream.net"
+  }
+
   $.get({
-    url: "https://encounter-service.dndbeyond.com/v1/encounters/54d34b6e-393a-4791-9ece-593b9c5745b1",
+    url: u,
     contentType: "application/json",
+    data: d,
     success: function(result) {
 	r = result
     },
@@ -16,5 +42,63 @@ function getEncounterDDB() {
     },
     async: false
   });
-  return r
+  return r;
+}
+
+function buildCombatants() {
+  var i = 0;
+  var c = [];
+  encounter.players.forEach(function (item, index) {
+    if (item.initiative > 0) {
+      item.active = true
+      char = getFromDDB("character", item.id);
+      var levels = 0;
+      var con_score = char.stats[2].value;
+      char.classes.forEach(function (it, ind) { levels += it.level });
+      char.modifiers.feat.forEach(function (it, ind) {
+	if (it.entityTypeId == 1472902489 && it.entityId == 3) { con_score += it.fixedValue }
+      });
+      char.modifiers.race.forEach(function (it, ind) {
+	if (it.entityTypeId == 1472902489 && it.entityId == 3) { con_score += it.fixedValue }
+      });
+      var con_mod = Math.floor((con_score - 10) / 2);
+      item.maximumHitPoints = char.baseHitPoints + (levels * con_mod) + char.temporaryHitPoints;
+      item.currentHitPoints = item.maximumHitPoints - char.removedHitPoints;
+      item.removedHitPoints = char.removedHitPoints;
+      item.overrideHitPoints = item.overrideHitPoints;
+      item.baseHitPoints = char.baseHitPoints;
+      item.currentXp = char.currentXp;
+      item.conditions = char.conditions;
+      item.deathSaves = char.deathSaves;
+      item.inspiration = char.inspiration;
+      item.portraitAvatarUrl = char.classes[0].definition.portraitAvatarUrl;
+    } else {
+      item.active = false
+    }
+    item.index = index;
+    item.type = "Player";
+    c[i] = item;
+    i += 1;
+  });
+  encounter.monsters.forEach(function (item, index) {
+    if (item.initiative > 0) { item.active = true } else { item.active = false }
+    item.index = index;
+    item.type = "Monster";
+    c[i] = item;
+    i += 1;
+  });
+  return c.sort(({initiative:a}, {initiative:b}) => b-a);
+}
+
+function populateOutput() {
+  var vis = "";
+  var o = $('#div-output');
+  combatants.forEach(function (item, index) {
+    var hp = "";
+    if (item.type == "Player" || opt_show_monster_name) { var t = item.name; } else { var t = item.type + ' ' + letters[item.index]; }
+    if (item.type == "Player" || opt_show_monster_hp) { hp = ' (' + item.currentHitPoints + '/' + item.maximumHitPoints + ')' }
+    if (item.initiative == 0) { vis = ' style="visibility:hidden;"' }
+    if (encounter.turnNum == index + 1) { var cls = ' class="combatant-current"' } else { var cls = ' class="combatant"' }
+    o.append('<div id="' + (index + 1) + '"' + vis + cls + '>' + item.initiative + ': ' + t + hp + '</div>');
+  });
 }
