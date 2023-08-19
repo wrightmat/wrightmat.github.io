b1654f9bca9e97782a38fe511ffd7db8d98641e4
Matt <wright.matt.a@gmail.com>
2023-08-19 12:38:15 -0500

    Dice roller

--- dnd/sheet.js
+++ dnd/sheet.js
@@ -1,5 +1,3 @@
-//const diceBox = window.diceBox;
-
 if ( window.diceBox == undefined ) {
 
   console.log("no dice box");
@@ -10,22 +8,21 @@ if ( window.diceBox == undefined ) {
     window.diceBox.roll('2d20')
   });
 
-
-window.diceBox.onRollComplete = function(rollResult) {
-  var rolls_val = 0;
-  var rolls_str = "";
-  console.log(rollResult);
-  rollResult[0].rolls.forEach(function (item, index) {
-    rolls_val += item.value;
-    if ( rolls_str != "" ) { rolls_str += " + " }
-    rolls_str += item.value;
-  });
-  rolls_str += ' = ';
-  var rolled = rollResult[0].qty + rollResult[0].sides;
-  if ( rollResult[0].modifier > 0 ) { rolled += " + " + rollResult[0].modifier; }
-  var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center', style: 'margin-left:-4px;padding-left:-4px;' }).appendTo('#results-list');
-  $('<span>', { style: 'font-size: 10px;', html: rolled + ': ' + rolls_str}).appendTo(li);
-  $('<span>', { class: 'badge badge-primary badge-pill', html: rolls_val }).appendTo(li);
-};
+  window.diceBox.onRollComplete = function(rollResult) {
+    var rolls_val = 0;
+    var rolls_str = "";
+    console.log(rollResult);
+    rollResult[0].rolls.forEach(function (item, index) {
+      rolls_val += item.value;
+      if ( rolls_str != "" ) { rolls_str += " + " }
+      rolls_str += item.value;
+    });
+    rolls_str += ' = ';
+    var rolled = rollResult[0].qty + rollResult[0].sides;
+    if ( rollResult[0].modifier > 0 ) { rolled += " + " + rollResult[0].modifier; }
+    var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center', style: 'margin-left:-4px;padding-left:-4px;' }).appendTo('#results-list');
+    $('<span>', { style: 'font-size: 10px;', html: rolled + ': ' + rolls_str}).appendTo(li);
+    $('<span>', { class: 'badge badge-primary badge-pill', html: rolls_val }).appendTo(li);
+  };
 
 }
