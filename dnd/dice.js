9a79f4dd17419bb1c6541546b977f0701b9dcfd7
Matt <wright.matt.a@gmail.com>
2023-08-18 22:23:28 -0500

    Update dice.js

--- dnd/dice.js
+++ dnd/dice.js
@@ -3,3 +3,7 @@ import DiceBox from 'dice-box'
 const diceBox = new DiceBox("#dice-box", {
   assetPath: '/assets/dice-box/' // include the trailing backslash
 })
+
+diceBox.init().then(()=>{
+  diceBox.roll('2d20')
+})
