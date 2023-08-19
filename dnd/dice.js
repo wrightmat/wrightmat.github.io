51423c8ab8bc09e548a51926622df286b1812c1d
Matt <wright.matt.a@gmail.com>
2023-08-19 12:50:32 -0500

    Update dice.js

--- dnd/dice.js
+++ dnd/dice.js
@@ -1,9 +1,7 @@
 import DiceBox from 'dice-box';
 
-const diceBox = new DiceBox("#dice-box", {
+export const diceBox = new DiceBox("#dice-box", {
   assetPath: '/dnd/assets/dice-box/',
   theme: "default",
   scale: 5
 });
-
-export diceBox;
