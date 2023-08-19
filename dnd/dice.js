d5c8e2bc442ab01c2c522fbd9ab35c1c1f2ebdee
Matt <wright.matt.a@gmail.com>
2023-08-19 11:15:18 -0500

    Typo

--- dnd/dice.js
+++ dnd/dice.js
@@ -23,6 +23,6 @@ diceBox.onRollComplete = function(rollResult) {
   var rolled = rollResult[0].qty + rollResult[0].sides;
   if ( rollResult[0].modifier > 0 ) { rolled += " + " + rollResult[0].modifier; }
   var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center' }).appendTo('#results-list');
-  $('<span>', { style: 'font-size: 8px;'html: rolled + ': ' + rolls_str}).appendTo(li);
+  $('<span>', { style: 'font-size: 8px;', 'html: rolled + ': ' + rolls_str}).appendTo(li);
   $('<span>', { class: 'badge badge-primary badge-pill', html: rolls_val }).appendTo(li);
 }
