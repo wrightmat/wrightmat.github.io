5e73880e5cb3d8075c99c2e623006b77776cd4ea
Matt <wright.matt.a@gmail.com>
2023-08-19 11:58:35 -0500

    Dice roller

--- dnd/dice.js
+++ dnd/dice.js
@@ -6,27 +6,4 @@ const diceBox = new DiceBox("#dice-box", {
   scale: 5
 });
 
-if ( diceBox ) {
-  diceBox.init().then(() => {
-    diceBox.roll('2d20')
-  });
-} else {
-  console.log("no dice box");
-}
-
-diceBox.onRollComplete = function(rollResult) {
-  var rolls_val = 0;
-  var rolls_str = "";
-  console.log(rollResult);
-  rollResult[0].rolls.forEach(function (item, index) {
-    rolls_val += item.value;
-    if ( rolls_str != "" ) { rolls_str += " + " }
-    rolls_str += item.value;
-  });
-  rolls_str += ' = ';
-  var rolled = rollResult[0].qty + rollResult[0].sides;
-  if ( rollResult[0].modifier > 0 ) { rolled += " + " + rollResult[0].modifier; }
-  var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center' style: 'margin-left:-4px;padding-left:-4px;' }).appendTo('#results-list');
-  $('<span>', { style: 'font-size: 10px;', html: rolled + ': ' + rolls_str}).appendTo(li);
-  $('<span>', { class: 'badge badge-primary badge-pill', html: rolls_val }).appendTo(li);
-}
+//window.diceBox = diceBox;
