b93287144d6cb3bdd88794fa8d4ceaa2fbaae600
Matt <wright.matt.a@gmail.com>
2023-08-19 14:35:21 -0500

    Dice roller

--- dnd/dice.js
+++ dnd/dice.js
@@ -13,26 +13,45 @@ function rollDice(notation) {
       arr.push(getRandomInt(d, die) * mult);
     }
 console.log(arr);
+    var rolls_val = 0;
+    var rolls_str = "";
+    arr.forEach(function (item, index) {
+      rolls_val += item.value;
+      if ( rolls_str != "" ) { rolls_str += " + " }
+      rolls_str += item.value;
+    }
+  displayDiceResults([ rolled, rolls_str, rolls_val, reason ]);
   } else {
     diceBox.init().then(() => {
       diceBox.roll(notation);
     });
-
     diceBox.onRollComplete = function(rollResult) {
-      var rolls_val = 0;
-      var rolls_str = "";
-      console.log(rollResult);
-      rollResult[0].rolls.forEach(function (item, index) {
-        rolls_val += item.value;
-        if ( rolls_str != "" ) { rolls_str += " + " }
-        rolls_str += item.value;
-      });
-      rolls_str += ' = ';
-      var rolled = rollResult[0].qty + rollResult[0].sides;
-      if ( rollResult[0].modifier > 0 ) { rolled += " + " + rollResult[0].modifier; }
-      var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center', style: 'margin-left:-4px;padding-left:-4px;' }).appendTo('#results-list');
-      $('<span>', { style: 'font-size: 10px;', html: rolled + ': ' + rolls_str}).appendTo(li);
-      $('<span>', { class: 'badge badge-primary badge-pill', html: rolls_val }).appendTo(li);
-    };
+      var arr = parseDice(rollResult);
+      displayDiceResults(arr);
+    }
   }
+}
+
+function parseDice(rollResult) {
+  var rolls_val = 0;
+  var rolls_str = "";
+
+  rollResult[0].rolls.forEach(function (item, index) {
+    rolls_val += item.value;
+    if ( rolls_str != "" ) { rolls_str += " + " }
+    rolls_str += item.value;
+  });
+  rolls_str += ' = ';
+
+  var rolled = rollResult[0].qty + rollResult[0].sides;
+  if ( rollResult[0].modifier > 0 ) { rolled += " + " + rollResult[0].modifier; }
+
+  return [ rolled, rolls_str, rolls_val, reason ]
+}
+
+function displayDiceResults(resultsArr) {
+console.log(resultsArr);
+  var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center', style: 'margin-left:-4px;padding-left:-4px;' }).appendTo('#results-list');
+  $('<span>', { style: 'font-size: 10px;', html: rolled + ': ' + rolls_str}).appendTo(li);
+  $('<span>', { class: 'badge badge-primary badge-pill', html: rolls_val }).appendTo(li);
 }
