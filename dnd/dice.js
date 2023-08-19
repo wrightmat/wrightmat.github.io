809dff9b719fee8f1285d5ab5b86a695fdea876c
Matt <wright.matt.a@gmail.com>
2023-08-19 11:34:27 -0500

    dice roller

--- dnd/dice.js
+++ dnd/dice.js
@@ -3,7 +3,7 @@ import DiceBox from 'dice-box'
 const diceBox = new DiceBox("#dice-box", {
   assetPath: '/dnd/assets/dice-box/',
   theme: "default",
-  scale: 9
+  scale: 6
 })
 
 diceBox.init().then(() => {
