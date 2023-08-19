4a5f48c368eec1219e75b3e5bfa8c215f7bf3421
Matt <wright.matt.a@gmail.com>
2023-08-18 23:38:24 -0500

    Update dice.js

--- dnd/dice.js
+++ dnd/dice.js
@@ -3,13 +3,13 @@ import DiceBox from 'dice-box'
 const diceBox = new DiceBox("#dice-box", {
   assetPath: '/dnd/assets/dice-box/',
   theme: "default",
-  scale: 15
+  scale: 16
 })
 
 diceBox.init().then(() => {
   diceBox.roll('2d20')
 })
 
-diceBox.onRollComplete = (rollResult) => function() {
+diceBox.onRollComplete => function(rollResult) {
   console.log('roll results', rollResult)
 }
