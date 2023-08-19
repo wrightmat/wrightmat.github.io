389e913aff87a97395de749f097ede34e6466346
Matt <wright.matt.a@gmail.com>
2023-08-19 17:29:27 -0500

    Update dice_3d.js

--- dnd/dice_3d.js
+++ dnd/dice_3d.js
@@ -12,7 +12,9 @@ diceBox.onRollComplete = function(rollResult) {
   var arr = parseDice(rollResult);
   displayDiceResults(arr);
   $('#dice-box').css('z-index', -1);
-  setTimeout(diceBox.clear(), 5000)
+  setInterval(function() {
+    diceBox.clear();
+  }, 5000);
 }
 
 window.rollDice3d = function (notation) {
