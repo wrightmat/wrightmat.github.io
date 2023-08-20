d4ed77f6a970cf1a8500502e4b2c5d6dd3287253
Matt <wright.matt.a@gmail.com>
2023-08-19 21:07:17 -0500

    Cleanup

--- dnd/dice.js
+++ dnd/dice.js
@@ -1,67 +1,71 @@
 function rollDice(notation, reason) {
-  var diceBox = window.diceBox;
+    var diceBox = window.diceBox;
 
-  if ( window.rollDice3d == undefined ) {
-    // if the 3d dice aren't initialized (local or an error) then fall back on a basic roller. 
-    var arr = [];
-    var d = notation.indexOf("d");
-    if ( notation.indexOf("x") >= 0 ) { var x = notation.indexOf("x") }
-    if ( notation.indexOf("+") >= 0 || notation.indexOf("-") >= 0 ) { var p = notation.indexOf("+") + notation.indexOf("-") + 1 }
-    var num = notation.substring(0, d) || 1;
-    if ( x || p ) {
-      var die = notation.substring(d + 1, ( x || p ));
-      if ( x ) {
-        var mult = notation.substring(x + 1, notation.length);
-        var plus = 0;
-      } else if ( p ) {
-	var plus = notation.substring(p + 1, notation.length);
-	var mult = 1;
-      }
+   if ( window.rollDice3d == undefined ) {
+	// if the 3d dice aren't initialized (local or an error) then fall back on a basic roller. 
+	var arr = [];
+	var d = notation.indexOf("d");
+	if ( notation.indexOf("x") >= 0 ) { var x = notation.indexOf("x") }
+	if ( notation.indexOf("+") >= 0 || notation.indexOf("-") >= 0 ) { var p = notation.indexOf("+") + notation.indexOf("-") + 1 }
+	var num = notation.substring(0, d) || 1;
+	if ( x || p ) {
+	    var die = notation.substring(d + 1, ( x || p ));
+	    if ( x ) {
+		var mult = notation.substring(x + 1, notation.length);
+		var plus = 0;
+	    } else if ( p ) {
+		var plus = notation.substring(p + 1, notation.length);
+		var mult = 1;
+	    }
+	} else {
+	    var die = notation.substring(d + 1, notation.length);
+	    var mult = 1;
+	    var plus = 0;
+	}
+
+	if ( parseInt(d) == 0 ) { d = 1; }
+	for ( let i = 0; i < num; i++ ) {
+	    arr.push(getRandomInt(d, die) * mult);
+	}
+
+	var rolls_val = parseInt(plus);
+	var rolls_str = "";
+	arr.forEach(function (item, index) {
+	    rolls_val += parseInt(item);
+	    if ( rolls_str != "" ) { rolls_str += " + " }
+	    rolls_str += item;
+	});
+
+	result = displayDiceResults([ notation, rolls_str, rolls_val, reason ]);
+
     } else {
-      var die = notation.substring(d + 1, notation.length);
-      var mult = 1;
-      var plus = 0;
+	result = window.rollDice3d(notation, reason);
     }
 
-    if ( parseInt(d) == 0 ) { d = 1; }
-    for ( let i = 0; i < num; i++ ) {
-      arr.push(getRandomInt(d, die) * mult);
-    }
-
-    var rolls_val = parseInt(plus);
-    var rolls_str = "";
-    arr.forEach(function (item, index) {
-      rolls_val += parseInt(item);
-      if ( rolls_str != "" ) { rolls_str += " + " }
-      rolls_str += item;
-    });
-
-    displayDiceResults([ notation, rolls_str, rolls_val, reason ]);
-
-  } else {
-    window.rollDice3d(notation, reason);
-  }
+    return result;
 }
 
 function parseDice(rollResult, reason) {
-  var rolls_val = 0;
-  var rolls_str = "";
+    var rolls_val = 0;
+    var rolls_str = "";
 
-  rollResult[0].rolls.forEach(function (item, index) {
-    if ( rolls_str != "" ) { rolls_str += " + " }
-    rolls_str += item.value;
-  });
-  rolls_val += rollResult[0].value
+    rollResult[0].rolls.forEach(function (item, index) {
+	if ( rolls_str != "" ) { rolls_str += " + " }
+	rolls_str += item.value;
+    });
+    rolls_val += rollResult[0].value
 
-  var rolled = rollResult[0].qty + rollResult[0].sides;
-  if ( rollResult[0].modifier > 0 ) { rolled += "+" + rollResult[0].modifier; }
+    var rolled = rollResult[0].qty + rollResult[0].sides;
+    if ( rollResult[0].modifier > 0 ) { rolled += "+" + rollResult[0].modifier; }
 
-  return [ rolled, rolls_str, rolls_val, reason ]
+    return [ rolled, rolls_str, rolls_val, reason ]
 }
 
 function displayDiceResults(resultsArr) {
-  if ( resultsArr[3] ) { var reason = resultsArr[3] + '<br />' } else { var reason = "" }
-  var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center' }).prependTo('#results-list');
-  $('<span>', { style: 'font-size: 10px;white-space: nowrap;', html: reason + '<b>' + resultsArr[0] + '</b>: ' + resultsArr[1]}).appendTo(li);
-  $('<span>', { class: 'badge badge-primary badge-pill', html: resultsArr[2] }).appendTo(li);
+    if ( resultsArr[3] ) { var reason = resultsArr[3] + '<br />' } else { var reason = "" }
+    var li = $('<li>', { class: 'list-group-item d-flex justify-content-between align-items-center' }).prependTo('#results-list');
+    $('<span>', { style: 'font-size: 10px;white-space: nowrap;', html: reason + '<b>' + resultsArr[0] + '</b>: ' + resultsArr[1]}).appendTo(li);
+    $('<span>', { class: 'badge badge-primary badge-pill', html: resultsArr[2] }).appendTo(li);
+
+    return resultsArr;
 }
--- dnd/dice_3d.js
+++ dnd/dice_3d.js
@@ -1,26 +1,25 @@
 import DiceBox from 'dice-box';
+var roll_reason;
 
 const diceBox = new DiceBox("#dice-box", {
-  assetPath: '/dnd/assets/dice-box/',
-  theme: "default",
-  scale: 5
+    assetPath: '/dnd/assets/dice-box/',
+    theme: "default",
+    scale: 5
 });
 
 diceBox.init()
 
-var roll_reason;
-
 diceBox.onRollComplete = function(rollResult) {
-  var arr = parseDice(rollResult, roll_reason);
-  displayDiceResults(arr);
-  $('#dice-box').css('z-index', -1);
+    var arr = parseDice(rollResult, roll_reason);
+    displayDiceResults(arr);
+    $('#dice-box').css('z-index', -1);
 }
 
 window.rollDice3d = function (notation, reason) {
-  roll_reason = reason;
-  $('#dice-box').css('z-index', 1);
-  diceBox.roll(notation);
-  setInterval(function() {
-    diceBox.clear();
-  }, 10000);
+    roll_reason = reason;
+    $('#dice-box').css('z-index', 1);
+    diceBox.roll(notation);
+    setInterval(function() {
+	diceBox.clear();
+    }, 8000);
 }
