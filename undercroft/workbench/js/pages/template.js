b3e17994efa28cfbd40b52d40d210c38bf289b55
Matt <wright.matt.a@gmail.com>
2025-10-17 17:56:27 -0500

    Refine character editor notes and workflows

--- undercroft/workbench/js/pages/template.js
+++ undercroft/workbench/js/pages/template.js
@@ -13,6 +13,7 @@ import { expandPane } from "../lib/panes.js";
 import { refreshTooltips } from "../lib/tooltips.js";
 import { resolveApiBase } from "../lib/api.js";
 import { BUILTIN_SYSTEMS, BUILTIN_TEMPLATES } from "../lib/content-registry.js";
+import { COMPONENT_ICONS, applyComponentStyles, applyTextFormatting } from "../lib/component-styles.js";
 
 (() => {
   const { status, undoStack } = initAppShell({ namespace: "template" });
@@ -23,19 +24,6 @@ import { BUILTIN_SYSTEMS, BUILTIN_TEMPLATES } from "../lib/content-registry.js";
   const systemCatalog = new Map();
 
   registerBuiltinContent();
-
-  const COMPONENT_ICONS = {
-    input: "tabler:forms",
-    array: "tabler:list-details",
-    divider: "tabler:separator-horizontal",
-    image: "tabler:photo",
-    label: "tabler:typography",
-    container: "tabler:layout-grid-add",
-    "linear-track": "tabler:timeline",
-    "circular-track": "tabler:gauge",
-    "select-group": "tabler:toggle-right",
-    toggle: "tabler:adjustments",
-  };
 
   const elements = {
     templateSelect: document.querySelector("[data-template-select]"),
@@ -1138,16 +1126,6 @@ import { BUILTIN_SYSTEMS, BUILTIN_TEMPLATES } from "../lib/content-registry.js";
     return wrapper;
   }
 
-  function applyComponentStyles(element, component) {
-    element.style.color = component.textColor || "";
-    element.style.backgroundColor = component.backgroundColor || "";
-    if (component.borderColor) {
-      element.style.borderColor = component.borderColor;
-    } else {
-      element.style.removeProperty("border-color");
-    }
-  }
-
   function renderComponentPreview(component) {
     switch (component.type) {
       case "input":
@@ -1716,44 +1694,6 @@ import { BUILTIN_SYSTEMS, BUILTIN_TEMPLATES } from "../lib/content-registry.js";
     wrapper.appendChild(preview);
 
     return wrapper;
-  }
-
-  function applyTextFormatting(element, component) {
-    if (!element) return;
-    const classes = [];
-    switch (component.textSize) {
-      case "sm":
-        classes.push("fs-6");
-        break;
-      case "lg":
-        classes.push("fs-5");
-        break;
-      case "xl":
-        classes.push("fs-4");
-        break;
-      default:
-        classes.push("fs-6");
-        break;
-    }
-    if (component.textStyles?.bold) {
-      classes.push("fw-semibold");
-    }
-    if (component.textStyles?.italic) {
-      classes.push("fst-italic");
-    }
-    if (component.textStyles?.underline) {
-      classes.push("text-decoration-underline");
-    }
-    element.classList.add(...classes);
-    if (component.align === "center") {
-      element.classList.add("text-center");
-    } else if (component.align === "end") {
-      element.classList.add("text-end");
-    } else if (component.align === "justify") {
-      element.style.textAlign = "justify";
-    } else {
-      element.classList.add("text-start");
-    }
   }
 
   function selectComponent(uid) {
